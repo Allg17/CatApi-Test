5bec4f94599b7820e984ab946f639def5e1b1851
Albert Virgilio Lopez Garcia <albert_lopez@ucne.edu.do>
2022-01-31 11:24:08 -0400

    Agregando Headers

--- CatApiApp/CatApiApp/Services/IimageApi.cs
+++ CatApiApp/CatApiApp/Services/IimageApi.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace CatApiApp.Services
 {
+    [Headers("Content-Type: application/json", "x-api-key: 42030dee-dc15-4010-a931-b9aaf28fbd09")]
     public interface IimageApi
     {
         /// <summary>
