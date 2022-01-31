5e0a7024632b0ddcd5908f351d1e43b3cd229087
Albert Virgilio Lopez Garcia <albert_lopez@ucne.edu.do>
2022-01-31 11:22:55 -0400

    Definiendo orientacion

--- CatApiApp/CatApiApp.Android/MainActivity.cs
+++ CatApiApp/CatApiApp.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Acr.UserDialogs;
 
 namespace CatApiApp.Droid
 {
-    [Activity(Label = "CatApiApp", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "CatApiApp", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
